b224d62c884da10e48281c06727752e327f34fcc
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2023-01-08 00:38:52 +0100

    form workin

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+import _ from 'lodash';
 
+const formElement = document.querySelector('.feedback-form');
+
+formElement.addEventListener('submit', e => {
+  e.preventDefault();
+  Array.from(formElement.elements).forEach(el => {
+    if (el.name) {
+      localStorage.setItem('feedback-form-state', JSON.stringify(el.value));
+    }
+  });
+});
