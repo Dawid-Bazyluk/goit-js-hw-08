41410af66d3b3a754a05c50a9740a58bbb13d4f1
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2023-01-08 01:51:28 +0100

    add storage key

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,12 @@
 import _ from 'lodash';
 
 const formElement = document.querySelector('.feedback-form');
-
+const storageKey = 'feedback-form-state';
 formElement.addEventListener('submit', e => {
   e.preventDefault();
   Array.from(formElement.elements).forEach(el => {
     if (el.name) {
-      localStorage.setItem('feedback-form-state', JSON.stringify(el.value));
+      localStorage.setItem(el.name, el.value);
     }
   });
 });
