f67bf7a80525465c7be36e2cd9e404136b476a7c
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2023-01-09 15:15:07 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,2 @@
 import _ from 'lodash';
 
-const formElement = document.querySelector('.feedback-form');
-const storageKey = 'feedback-form-state';
-formElement.addEventListener('submit', e => {
-  e.preventDefault();
-  Array.from(formElement.elements).forEach(el => {
-    if (el.name) {
-      localStorage.setItem(el.name, el.value);
-    }
-  });
-});
