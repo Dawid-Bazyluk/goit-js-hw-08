b148a07c7e93712ed8a29379aaecfef5eeda2b34
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2023-01-09 12:20:01 +0100

    added video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+const getVideoCurrentTime = () => {
+  return localStorage.getItem(STORAGE_KEY);
+};
+const onSaveTimeUpdate = ({ seconds } = 0) => {
+  console.log(seconds);
+  localStorage.setItem(STORAGE_KEY, seconds);
+};
+
+player.setCurrentTime(getVideoCurrentTime());
+
+player.on('timeupdate', _.throttle(onSaveTimeUpdate, 1000));
